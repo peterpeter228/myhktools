3faeb5253de93411fa8abd0f978e4fdbe0b59654
MTX_TianXia <11602011@qq.com>
2017-07-17 22:21:33 +0800

    gbk2utf8.js 支持目录

--- gbk2utf8.js
+++ gbk2utf8.js
@@ -1,8 +1,38 @@
+// 别忘记了，后渗透meterpreter会产生大量的gbk文件，在mac osx、linux下无法正常阅读
 var a = process.argv.splice(2),
 	fs = require('fs'),
-    k = fs.readFileSync(a[0]),
     iconv = require("iconv-lite");
-k = iconv.decode(k,"gbk").toString("utf8");
 
-fs.writeFileSync(a[0],k);
-console.log(k);
+function doFile(filename)
+{
+	fs.stat(filename,function(e,stats)
+	{
+		if(stats.isFile() && /\.(txt|log)/gmi.test(filename) && fs.existsSync(filename))
+		{
+			try{
+				var k = fs.readFileSync(filename);
+				k = iconv.decode(k,"gbk").toString("utf8");
+				fs.writeFileSync(filename,k);
+				console.log(filename);
+				console.log(k);
+			}catch(e1){console.log(e1);}
+		}
+		else if(stats.isDirectory())
+		{
+			fs.readdir(filename,{},function(e,aF)
+			{
+				aF.forEach(function(i)
+				{
+					if(".DS_Store" == i)
+					{
+						fs.unlinkSync(filename + "/" + i);
+					}
+					else doFile(filename + "/" + i)；
+				});
+			});
+		}
+	});
+}
+
+doFile(a[0]);
+
