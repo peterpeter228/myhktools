5a8480a2f5f7c76eb0c2ecab19e7f23597cedaa0
MTX_TianXia <11602011@qq.com>
2018-01-26 14:43:40 +0800

    add s2-053

--- test/testAll.sh
+++ test/testAll.sh
@@ -1,15 +1,18 @@
 node checkUrl.js -u http://192.168.10.216:8088/S2-001/ --struts2 001
+# node checkUrl.js -u http://192.168.10.216:8088/S2-005/ --struts2 005
 node checkUrl.js -u http://192.168.10.216:8088/S2-007/ --struts2 007
 node checkUrl.js -u http://192.168.10.216:8088/S2-008/ --struts2 008
 node checkUrl.js -u http://192.168.10.216:8088/S2-012/ --struts2 012
 node checkUrl.js -u http://192.168.10.216:8088/S2-013/ --struts2 013
 node checkUrl.js -u http://192.168.10.216:8088/S2-015/ --struts2 015
-node checkUrl.js -u http://192.168.10.216:8088/S2-019/ --struts2 019
 node checkUrl.js -u http://192.168.10.216:8088/S2-016/default.action --struts2 016
+node checkUrl.js -u http://192.168.10.216:8088/S2-019/ --struts2 019
 node checkUrl.js -u http://192.168.10.216:8088/S2-029/default.action --struts2 029
 node checkUrl.js -u http://192.168.10.216:8088/s2-032/memoindex.action --struts2 032
+# node checkUrl.js -u http://192.168.10.216:8088/s2-033/ --struts2 033
+# node checkUrl.js -u http://192.168.10.216:8088/s2-037/ --struts2 037
 node checkUrl.js -u http://192.168.10.216:8082/s2-032 --struts2 045
 node checkUrl.js -u http://192.168.10.216:8082/s2-046/doUpload.action --struts2 046
 node checkUrl.js -u http://192.168.10.216:8082/s2-048/integration/saveGangster.action --struts2 048
-
+node checkUrl.js -u http://192.168.10.216:8082/s2-053/ --struts2 053
 
